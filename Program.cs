1feb97d5b6040773b353820c8c7170b35b85c1db
Hugo Vallada <valladahugo@gmail.com>
2020-12-06 19:32:12 -0300

    Commit inicial do projeto de file-renamer com c#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace batch_file_renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
